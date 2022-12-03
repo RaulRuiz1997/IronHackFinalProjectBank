52bff8394642e682474d74ad8266a921facc6e1e
Raúl Ruiz <raul.ruizpedrosa@Outlook.com>
2022-12-03 15:24:45 +0100

    Inicio proyecto

---
+++ src/main/java/com/IronHackRaulRuiz/FinalProjectRaulRuiz/FinalProjectRaulRuizApplication.java
@@ -0,0 +1,13 @@
+package com.IronHackRaulRuiz.FinalProjectRaulRuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectRaulRuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectRaulRuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IronHackRaulRuiz/FinalProjectRaulRuiz/FinalProjectRaulRuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IronHackRaulRuiz.FinalProjectRaulRuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectRaulRuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
